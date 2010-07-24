fea1abc060cb1979c315e18223fb3d8b3395fd12
Hemant Kumar <gethemant@gmail.com>
2010-07-25 04:24:50 +0530

    add seed data and stuff

--- public/javascripts/roadtrip.js
+++ public/javascripts/roadtrip.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 });
 
 $('a#submit_link').live('click',function() {
-    var userChoice = $("form#question_form input:radio[name=gender]:checked").val();
+    var userChoice = $("form#question_form input:radio[name=answer]:checked").val();
     var jsonResponse = {
 	'question_id': $('#question_id').val(),
 	'option': userChoice,
