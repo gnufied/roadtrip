244b1526d16162de1bbf5f841dc6080bebe2a980
Prateek Dayal <prateek@muziboo.com>
2010-07-25 04:12:59 +0530

    mostly everything working now

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,7 @@ class QuestionsController < ApplicationController
 
   def create
     @question = Question.create(params[:question])
+    render :nothing => true
   end
 
 end
--- public/javascripts/quiz.js
+++ public/javascripts/quiz.js
@@ -3,7 +3,7 @@ var Quiz = {
   generateIdeas: function(){
         var query = jQuery("#idea_keyword").attr("value");
         jQuery.yql(            
-            "select * from xml where url='http://en.wikipedia.org/w/api.php?action=opensearch&search="+query+"&format=xml' and itemPath = 'SearchSuggestion.Section.Item'", {},
+            "select * from xml where url='http://en.wikipedia.org/w/api.php?action=opensearch&search="+escape(query)+"&format=xml' and itemPath = 'SearchSuggestion.Section.Item' limit 5", {},
             function (data) {
                 if (data.query.results.Item.length > 0) {
                   var suggestions_html = "";
@@ -18,14 +18,12 @@ var Quiz = {
             }
           );
         jQuery.yql(
-            "select * from flickr.photos.search where text=#{query}", {query: query},
+            "select * from flickr.photos.search where text=#{query} limit 9", {query: query},
             function(data) {  
               if(data.query.results.photo.length >0) {
                 var suggestions_image_html = "";
                 jQuery.each(data.query.results.photo, function(index, value){
-                    suggestions_image_html += "<p>";
                     suggestions_image_html += "<img src='http://farm"+value.farm+".static.flickr.com/"+value.server+"/"+value.id+"_"+value.secret+"_s.jpg'/>";
-                    suggestions_image_html += "</p>";
                 });
                   jQuery("#question_ideas_images").html(suggestions_image_html);
               }
